fbe9f36dd60088b5318af52d380afbcf5310be50
coderstephen <me@stephencoakley.com>
2014-12-31 20:35:07 -0600

    Better ls command that is sorted by type

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -51,7 +51,7 @@ function weather
 end
 
 function ls --description "Show more detailed 'ls' output"
-    command ls -l -a --human-readable --classify --color=always $argv | less -XRF
+    command ls -l -A --human-readable --classify --color=always --group-directories-first $argv | less -XRF
 end
 
 function fish_greeting
