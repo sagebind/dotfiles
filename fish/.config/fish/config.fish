43c1ec59d3478953a9ccb903639d8d98dff98f2e
coderstephen <me@stephencoakley.com>
2014-12-31 21:06:00 -0600

    Show git info in prompt

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,11 @@
+# color variables
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
+
 # common root commands
 function reboot
     sudo reboot
@@ -60,4 +68,40 @@ function fish_greeting
     if begin; [ "$TERM" = "screen" ]; not set -q TMUX; end
         tmux
     end
+end
+
+# Fish git prompt
+set __fish_git_prompt_show_informative_status 'yes'
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '☡'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
+function fish_prompt
+    set last_status $status
+
+    # display user and host name
+    printf "%s@%s" (whoami) (hostname)
+
+    # display working directory
+    set_color $fish_color_cwd
+    printf ' %s' (prompt_pwd)
+    set_color normal
+
+    # display git status
+    printf '%s' (__fish_git_prompt)
+
+    printf '> '
+    set_color normal
 end
