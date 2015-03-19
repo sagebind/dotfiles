6d1f60387f18208f326fe14a19c651ce653b54bb
coderstephen <me@stephencoakley.com>
2015-03-18 20:03:22 -0500

    Added handy wrapper around geeknote

---
+++ fish/.config/fish/functions/en.fish
@@ -0,0 +1,14 @@
+function en -d "Simple wrapper around Geeknote"
+    if test (count $argv) -lt 1
+        echo "No command specified."
+        return 1
+    end
+    switch $argv[1]
+        case list
+            geeknote --help
+        case show
+            geeknote show $argv[2..-1] | markdown2 | lynx -stdin -nomargins -nomore -notitle
+        case '*'
+            geeknote $argv[1..-1]
+    end
+end
