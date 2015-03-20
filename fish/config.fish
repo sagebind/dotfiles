406eda5e8b6581256b456ec40beafc82a3ebd030
coderstephen <me@stephencoakley.com>
2015-03-20 12:27:44 -0500

    Two new navigation-related plugins and switched theme

--- fish/config.fish
+++ fish/config.fish
@@ -1,22 +1,20 @@
 # always run inside tmux
 function fish_greeting
     if begin; [ "$TERM" = "screen" ]; not set -q TMUX; end
-        tmux; and exit
+        tmux -2; and exit
     end
 end
 
-# Path to your oh-my-fish.
+# set the oh-my-fish config path
 set fish_path $HOME/.oh-my-fish
 
-# Theme
-set fish_theme robbyrussell
+# use the "l" fish theme and some options
+set fish_theme l
 
-# All built-in plugins can be found at ~/.oh-my-fish/plugins/
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Enable plugins by adding their name separated by a space to the line below.
-set fish_plugins theme composer evernote
+# set some plugins to be loaded
+set fish_plugins theme autojump bd composer evernote
 
-# Load oh-my-fish configuration.
+# load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
 
 function reboot
