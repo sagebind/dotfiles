e30544ea84c14f240a2f190f501770a863aaa018
coderstephen <me@stephencoakley.com>
2015-06-13 10:59:02 -0500

    Better, more consistent Conky widget coloring

--- conky/conky.lua
+++ conky/conky.lua
@@ -1,14 +1,14 @@
     --[[
     Ring Meters by londonali1010 (2009)
-     
+
     This script draws percentage meters as rings. It is fully customisable; all options are described in the script.
-     
+
     IMPORTANT: if you are using the 'cpu' function, it will cause a segmentation fault if it tries to draw a ring straight away. The if statement on line 145 uses a delay to make sure that this doesn't happen. It calculates the length of the delay by the number of updates since Conky started. Generally, a value of 5s is long enough, so if you update Conky every 1s, use update_num > 5 in that if statement (the default). If you only update Conky every 2s, you should change it to update_num > 3; conversely if you update Conky every 0.5s, you should use update_num > 10. ALSO, if you change your Conky, is it best to use "killall conky; conky" to update it, otherwise the update_num will not be reset and you will get an error.
-     
+
     To call this script in Conky, use the following (assuming that you save this script to ~/scripts/rings.lua):
             lua_load ~/scripts/rings-v1.2.1.lua
             lua_draw_hook_pre ring_stats
-     
+
     Changelog:
     + v1.2.2 -- Fixed memory leak (by AmIMeYet)
     + v1.2.1 -- Fixed minor bug that caused script to crash if conky_parse() returns a nil value (20.10.2009)
@@ -16,7 +16,7 @@
     + v1.1 -- Added options for the starting angle of the rings, and added the "max" variable, to allow for variables that output a numerical value rather than a percentage (29.09.2009)
     + v1.0 -- Original release (28.09.2009)
     ]]
-     
+
     settings_table = {
             {
                     -- Edit this table to customise your rings.
@@ -30,9 +30,9 @@
                     -- "bg_colour" is the colour of the base ring.
                     bg_colour=0x000000,
                     -- "bg_alpha" is the alpha value of the base ring.
-                    bg_alpha=0.1,
+                    bg_alpha=0.3,
                     -- "fg_colour" is the colour of the indicator part of the ring.
-                    fg_colour=0xffffff,
+                    fg_colour=0x009CDE,
                     -- "fg_alpha" is the alpha value of the indicator part of the ring.
                     fg_alpha=0.6,
                     -- "x" and "y" are the x and y coordinates of the centre of the ring, relative to the top left corner of the Conky window.
@@ -52,8 +52,8 @@
                     arg='%M.%S',
                     max=60,
                     bg_colour=0x000000,
-                    bg_alpha=0.1,
-                    fg_colour=0xffffff,
+                    bg_alpha=0.3,
+                    fg_colour=0x009CDE,
                     fg_alpha=0.6,
                     x=100, y=120,
                     radius=65,
@@ -67,8 +67,8 @@
                     arg='%S',
                     max=60,
                     bg_colour=0x000000,
-                    bg_alpha=0.1,
-                    fg_colour=0xffffff,
+                    bg_alpha=0.3,
+                    fg_colour=0x009CDE,
                     fg_alpha=0.6,
                     x=100, y=120,
                     radius=50,
@@ -83,7 +83,7 @@
                     max=100,
                     bg_colour=0x000000,
                     bg_alpha=0.3,
-                    fg_colour=0xFFFFFF,
+                    fg_colour=0x009CDE,
                     fg_alpha=0.6,
                     x=100, y=350,
                     radius=78,
@@ -98,7 +98,7 @@
                     max=100,
                     bg_colour=0x000000,
                     bg_alpha=0.3,
-                    fg_colour=0xFFFFFF,
+                    fg_colour=0x009CDE,
                     fg_alpha=0.6,
                     x=100, y=506,
                     radius=78,
@@ -113,7 +113,7 @@
                     max=100,
                     bg_colour=0x000000,
                     bg_alpha=0.3,
-                    fg_colour=0xffffff,
+                    fg_colour=0x009CDE,
                     fg_alpha=0.6,
                     x=256, y=506,
                     radius=78,
@@ -184,53 +184,53 @@
             ]]--
             ,
     }
-     
+
     require 'cairo'
-     
+
     function rgb_to_r_g_b(colour,alpha)
             return ((colour / 0x10000) % 0x100) / 255., ((colour / 0x100) % 0x100) / 255., (colour % 0x100) / 255., alpha
     end
-     
+
     function draw_ring(cr,t,pt)
             local w,h=conky_window.width,conky_window.height
-     
+
             local xc,yc,ring_r,ring_w,sa,ea=pt['x'],pt['y'],pt['radius'],pt['thickness'],pt['start_angle'],pt['end_angle']
             local bgc, bga, fgc, fga=pt['bg_colour'], pt['bg_alpha'], pt['fg_colour'], pt['fg_alpha']
-     
+
             local angle_0=sa*(2*math.pi/360)-math.pi/2
             local angle_f=ea*(2*math.pi/360)-math.pi/2
             local t_arc=t*(angle_f-angle_0)
-     
+
             -- Draw background ring
-     
+
             cairo_arc(cr,xc,yc,ring_r,angle_0,angle_f)
             cairo_set_source_rgba(cr,rgb_to_r_g_b(bgc,bga))
             cairo_set_line_width(cr,ring_w)
             cairo_stroke(cr)
-     
+
             -- Draw indicator ring
-     
+
             cairo_arc(cr,xc,yc,ring_r,angle_0,angle_0+t_arc)
             cairo_set_source_rgba(cr,rgb_to_r_g_b(fgc,fga))
-            cairo_stroke(cr)               
+            cairo_stroke(cr)
     end
-     
+
     cs, cr = nil -- initialize our cairo surface and context to nil
     function conky_ring_stats()
             local function setup_rings(cr,pt)
                     local str=''
                     local value=0
-     
+
                     str=string.format('${%s %s}',pt['name'],pt['arg'])
                     str=conky_parse(str)
-     
+
                     value=tonumber(str)
                     if value == nil then value = 0 end
                     pct=value/pt['max']
-     
+
                     draw_ring(cr,pct,pt)
             end
-     
+
             if conky_window == nil then return end
             if cs == nil or cairo_xlib_surface_get_width(cs) ~= conky_window.width or cairo_xlib_surface_get_height(cs) ~= conky_window.height then
                     if cs then cairo_surface_destroy(cs) end
@@ -238,16 +238,16 @@
             end
             if cr then cairo_destroy(cr) end
             cr = cairo_create(cs)
-     
+
             local updates=conky_parse('${updates}')
             update_num=tonumber(updates)
-     
+
             if update_num>5 then
                     for i in pairs(settings_table) do
                             setup_rings(cr,settings_table[i])
                     end
             end
-           
+
             cairo_destroy(cr)
             cr = nil
     end
