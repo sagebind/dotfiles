adbd0d35cd7601006cbe690d37eab0a2f0f59c5f
coderstephen <me@stephencoakley.com>
2015-07-31 14:26:06 -0500

    Update to align with left screen edge

--- devilspie2/desktop-term.lua
+++ devilspie2/desktop-term.lua
@@ -1,5 +1,5 @@
 if (get_window_role() == "desktop-term") then
-   set_window_geometry(84,14,1540,1032);
+   set_window_geometry(14,14,1610,1032);
    undecorate_window();
    set_skip_pager(true);
    set_skip_tasklist(true);
