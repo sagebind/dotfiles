b7294845e73811f6fb4a2c89494fbecfabe8f18d
coderstephen <me@stephencoakley.com>
2015-08-05 18:00:04 -0500

    Fix positioning on multi-monitor

--- devilspie2/desktop-term.lua
+++ devilspie2/desktop-term.lua
@@ -3,7 +3,7 @@ if (get_window_role() == "desktop-term") then
    undecorate_window();
    set_skip_pager(true);
    set_skip_tasklist(true);
-   set_window_type("WINDOW_TYPE_DESKTOP");
+   set_window_type("WINDOW_TYPE_UTILITY");
    set_window_below();
    pin_window();
    stick_window();
