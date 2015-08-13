e2a4402e79a81bd7310791709af46dffb2e1832b
coderstephen <me@stephencoakley.com>
2015-08-12 23:54:07 -0500

    Fix fish function and enable word wrap in Atom

--- fish/functions/fix-chrome-shortcut.fish
+++ fish/functions/fix-chrome-shortcut.fish
@@ -1,1 +1,3 @@
-sudo sed -i -- 's/usr\/bin\/google-chrome-stable/opt\/google\/chrome\/google-chrome/g' /usr/share/applications/google-chrome.desktop
+function fix-chrome-shortcut
+    sudo sed -i -- 's/usr\/bin\/google-chrome-stable/opt\/google\/chrome\/google-chrome/g' /usr/share/applications/google-chrome.desktop
+end
