3e111173dfece7e6d8147d899fd71fa287267d30
Stephen Coakley <me@stephencoakley.com>
2015-09-03 12:52:49 -0500

    Better tmux session handling & statusbar changes

--- omf/pkg/tmux/tmux.fish
+++ omf/pkg/tmux/tmux.fish
@@ -1,9 +1,16 @@
 function init --on-event init_tmux
     function fish_greeting
         if begin; [ "$TERM" = "screen" ]; not set -q TMUX; end
-            tmux new-session -t local \; new-window
-            or tmux new-session -s local
+            __tmux_link_session local
             and exit
+        else
+            # Set useful tmux variables.
+            set -g TMUX_WINDOW (tmux display-message -p '#I')
         end
     end
+end
+
+function __tmux_link_session -d "Starts a tmux session linked to the given session name"
+    tmux new-session -t $argv[1] \; set-option destroy-unattached on \; new-window
+    or tmux new-session -s $argv[1]
 end
