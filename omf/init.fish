3030dc90f8d69d50ff8824aba2dc94c3eef96873
Stephen Coakley <me@stephencoakley.com>
2015-11-29 23:07:31 -0600

    Uprage level of tmux awesomeness

--- omf/init.fish
+++ omf/init.fish
@@ -4,3 +4,10 @@ source /usr/share/autojump/autojump.fish
 # Hide greeting
 function fish_greeting
 end
+
+# Better titles
+function fish_title
+  test (count $argv) -gt 0
+    and echo $argv[1]
+    or echo $_
+end
