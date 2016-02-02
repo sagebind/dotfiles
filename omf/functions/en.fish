9b7b9e74da69e39b860b4ea79a353338c98dec7a
Stephen Coakley <me@stephencoakley.com>
2016-02-02 13:32:52 -0600

    Enhance searching syntax

--- omf/functions/en.fish
+++ omf/functions/en.fish
@@ -8,6 +8,39 @@ function en -d "Command for accessing Evernote using Geeknote"
             geeknote $argv
             geeknote find
             geeknote edit 1
+        case find
+            set -l search
+            set -l tags
+            set -l notebook
+            set -l count 20
+
+            for arg in $argv[2..-1]
+                if test (echo $arg | cut -c 1-4) = "tag:"
+                    set tags $tags (echo $arg | cut -c 5-)
+                else if test (echo $arg | cut -c 1-9) = "notebook:"
+                    set notebook (echo $arg | cut -c 10-)
+                else if test (echo $arg | cut -c 1-6) = "count:"
+                    set count (echo $arg | cut -c 7-)
+                else
+                    set search $search $arg
+                end
+            end
+
+            set -l params find --content-search --count $count
+
+            if test -n "$notebook"
+                set params $params --notebooks "$notebook"
+            end
+
+            if test -n "$tags"
+                set params $params --tags (echo -s ,$tags | cut -b 2-)
+            end
+
+            if test -n "$search"
+                set params $params --search "$search"
+            end
+
+            geeknote $params
         case list
             geeknote --help
         case manual
