6f1d648defb3b043f3a191ae982253f882f3490a
Stephen Coakley <me@stephencoakley.com>
2016-04-06 11:10:07 -0500

    Add/improve utility functions

--- omf/functions/pgrep.fish
+++ omf/functions/pgrep.fish
@@ -1,3 +1,3 @@
-function pgrep -d 'Find a process' -a pattern
-  ps -eo user,pid,%cpu,vsz,tty,args | fzf --multi --header-lines=1 --nth=1,2,5,6 --query="$pattern" | awk '{ print $2 }'
+function pgrep -d 'Find a process'
+  ps -eo user,pid,%cpu,vsz,tty,args | fzf --multi --header-lines=1 --select-1 --nth=1,2,5,6 --query="$argv" | awk '{ print $2 }'
 end
