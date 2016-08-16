06a3df0247c1b253702896e7d842cf954a3b7892
Stephen M. Coakley <me@stephencoakley.com>
2016-08-16 11:24:39 -0500

    Consolidate fish config

--- omf/before.init.fish
+++
@@ -1,9 +0,0 @@
-# Load env variables
-if test -f ~/.env.fish
-    source ~/.env.fish
-end
-
-# Set the terminal window to use a dark GTK theme.
-if begin; type -q xprop; and set -q WINDOWID; end
-    xprop -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT "dark" -id $WINDOWID
-end
