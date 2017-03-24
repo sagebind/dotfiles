035b850bfec2b6a6d860562c720fa37bb735e709
Stephen M. Coakley <me@stephencoakley.com>
2017-03-23 21:37:48 -0500

    Merge in vscode settings from Ashbite

--- fish/functions/bluetooth-headset.fish
+++ fish/functions/bluetooth-headset.fish
@@ -1,28 +1,35 @@
 function bluetooth-headset -d 'Connect a bluetooth audio headset'
   pulseaudio -k
 
-  for i in (seq 5)
-    echo "$i out of 5 tries..."
-    echo "Go ahead and turn on the headset. Press any key after it is connected. "
-    read -p '' -n 1
-    sleep 1
+  # Set profile to A2DP
+  while true
+    echo -n "Waiting for headset to connect... "
+    while not hcitool con | grep 'ACL' > /dev/null ^&2
+      sleep 1
+    end
+    echo "Done"
 
-    set -l card_number (pactl list cards short | grep 'bluez_card' | tail -n 1 | awk '{print $1}')
+    echo -n "Getting card address... "
+    sleep 20
+    set -l card_number (pactl list cards short | grep 'bluez_card' | tail -n 1 | awk '{print $2}')
 
     if test -z "$card_number"
-      echo "Headset profile not connected."
-      echo "Trying to reconnect..."
-      sudo service bluetooth restart
-      continue
+      echo "Profile not connected"
+    else
+      echo $card_number
+
+      echo -n "Setting card profile... "
+      if pactl set-card-profile $card_number a2dp_sink
+        echo "Done"
+        break
+      end
     end
 
-    if pactl set-card-profile $card_number a2dp_sink
-      echo "Headset connected successfully!"
-      return
-    end
-
-    echo "Failed to set headset profile to A2DP."
     echo "Retrying..."
     sudo service bluetooth restart
   end
+
+  # Set default sink
+  set -l sink (pactl list sinks short | grep 'bluez_sink' | tail -n 1 | awk '{print $2}')
+  pactl set-default-sink $sink
 end
