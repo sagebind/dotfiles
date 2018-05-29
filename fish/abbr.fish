a4a6097d23e2f46b8b5932f0c99790f26b86ba3c
Stephen M. Coakley <me@stephencoakley.com>
2018-05-29 10:58:03 -0500

    Add aliases to config

---
+++ fish/abbr.fish
@@ -0,0 +1,17 @@
+if status --is-interactive
+  set -g fish_user_abbreviations
+
+  abbr d docker
+  abbr dc docker-compose
+  abbr dm docker-machine
+
+  abbr g git
+  abbr gc 'git checkout'
+  abbr gcd 'git checkout develop'
+  abbr gcm 'git checkout master'
+  abbr gm 'git merge'
+  abbr gp 'git pull'
+  abbr gs 'git s'
+
+  abbr s3 'aws s3'
+end
