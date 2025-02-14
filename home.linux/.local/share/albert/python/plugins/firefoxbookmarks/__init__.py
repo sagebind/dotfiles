0f8cfc305d94225b87e46510a13ff4d21ac871fc
Stephen M. Coakley <me@stephencoakley.com>
2025-02-14 12:11:27 -0600

    Support searching arbitrarily nested Firefox bookmarks

--- home.linux/.local/share/albert/python/plugins/firefoxbookmarks/__init__.py
+++ home.linux/.local/share/albert/python/plugins/firefoxbookmarks/__init__.py
@@ -19,15 +19,64 @@ class Plugin(PluginInstance, TriggerQueryHandler):
             self, self.id, self.name, self.description,
             defaultTrigger='bm '
         )
-        # GlobalQueryHandler.__init__(self, self.id, self.name, self.description, defaultTrigger='st ')
         self.fbh = FBH(self)
         self.registerExtension(self.fbh)
+
+        self._include_toolbar = self.readConfig("include_toolbar", bool) or True
+        self._include_menu = self.readConfig("include_menu", bool) or False
+        self._include_unfiled = self.readConfig("include_unfiled", bool) or False
 
         self.firefox_root = Path.home() / ".mozilla" / "firefox"
         self.resolved_places_path = str(self._get_places_path())
 
     def __del__(self):
         self.deregisterExtension(self.fbh)
+
+    @property
+    def include_toolbar(self):
+        return self._include_toolbar
+
+    @include_toolbar.setter
+    def include_toolbar(self, value):
+        self._include_toolbar = value
+        self.writeConfig("include_toolbar", value)
+
+    @property
+    def include_menu(self):
+        return self._include_menu
+
+    @include_menu.setter
+    def include_menu(self, value):
+        self._include_menu = value
+        self.writeConfig("include_menu", value)
+
+    @property
+    def include_unfiled(self):
+        return self._include_unfiled
+
+    @include_unfiled.setter
+    def include_unfiled(self, value):
+        self._include_unfiled = value
+        self.writeConfig("include_unfiled", value)
+
+    def configWidget(self):
+        return [
+            {
+                'type': 'checkbox',
+                'property': 'include_toolbar',
+                'label': 'Include bookmarks in "Bookmarks Toolbar" in results.',
+            },
+            {
+                'type': 'checkbox',
+                'property': 'include_menu',
+                'label': 'Include bookmarks in "Bookmarks Menu" in results.',
+            },
+            {
+                'type': 'checkbox',
+                'property': 'include_unfiled',
+                'label': 'Include bookmarks in "Other Bookmarks" in results.',
+            },
+        ]
 
     def handleTriggerQuery(self, query):
         for item in self._query(query.string, 5):
@@ -36,7 +85,7 @@ class Plugin(PluginInstance, TriggerQueryHandler):
     def _query(self, term, limit):
         places = Places(self.resolved_places_path)
         try:
-            for title, url, iconUrl in places.query(term, limit):
+            for title, url, iconUrl in places.query(term, self._get_root_folders(), limit):
                 yield StandardItem(
                     id=url,
                     text=title,
@@ -52,6 +101,16 @@ class Plugin(PluginInstance, TriggerQueryHandler):
                 )
         finally:
             places.close()
+
+    def _get_root_folders(self):
+        folders = []
+        if self.include_toolbar:
+            folders.append("toolbar")
+        if self.include_menu:
+            folders.append("menu")
+        if self.include_unfiled:
+            folders.append("unfiled")
+        return folders
 
     def _get_places_path(self):
         profile_id = self._get_default_profile_id()
@@ -76,29 +135,39 @@ class FBH(FallbackHandler):
 class Places:
     def __init__(self, path):
         self.conn = sqlite3.connect(f"file:{path}?immutable=1", uri=True)
-        # self.conn = sqlite3.connect(path)
 
-    def query(self, term, limit):
+    def query(self, term, parent_folders, limit):
         c = self.conn.cursor()
-        c.execute(
-            """
-                SELECT bookmark.title, place.url, place.preview_image_url
-                FROM moz_bookmarks bookmark
-                JOIN moz_places place ON place.id = bookmark.fk
-                JOIN moz_bookmarks parent1 ON parent1.id = bookmark.parent
-                JOIN moz_bookmarks parent2 ON parent2.id = parent1.parent
-                LEFT JOIN moz_bookmarks parent3 ON parent3.id = parent2.parent
-                WHERE
-                    bookmark.title LIKE ?
-                    AND (
-                        (parent1.title = "toolbar" AND parent2.parent = 0)
-                        OR (parent2.title = "toolbar" AND parent3.parent = 0)
-                    )
-                ORDER BY place.frecency DESC
-                LIMIT ?
-            """,
-            (f"%{term}%", limit)
-        )
+
+        params = [f"%{term}%"]
+        params.extend(parent_folders)
+        params.append(limit)
+
+        # This crazy query recursively traverses the bookmark tree to filter
+        # bookmarks by their root folder, then sorting the results by "frecency".
+        query = f"""
+            WITH RECURSIVE bookmarks_tree(bookmark_id, root_id) AS (
+                SELECT id AS bookmark_id, id AS root_id
+                FROM moz_bookmarks
+                WHERE parent = 1
+                UNION ALL
+                SELECT b.id AS bookmark_id, tree.root_id
+                FROM moz_bookmarks b
+                JOIN bookmarks_tree tree ON b.parent = tree.bookmark_id
+            )
+            SELECT bookmark.title, place.url, place.preview_image_url
+            FROM bookmarks_tree tree
+            JOIN moz_bookmarks bookmark ON bookmark.id = tree.bookmark_id
+            JOIN moz_bookmarks root ON root.id = tree.root_id
+            JOIN moz_places place ON place.id = bookmark.fk
+            WHERE tree.bookmark_id != tree.root_id
+                AND bookmark.title LIKE ?
+                AND root.title IN ({','.join('?' * len(parent_folders))})
+            ORDER BY place.frecency DESC
+            LIMIT ?
+        """
+
+        c.execute(query, params)
         return c.fetchall()
 
     def close(self):
