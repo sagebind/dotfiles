610b0860811d71b622c4481ddf91157d434b9d41
Stephen M. Coakley <me@stephencoakley.com>
2025-02-19 09:57:47 -0600

    Ensure flox paths are set up before other configs

--- home/.config/fish/conf.d/00-flox.fish
+++ home/.config/fish/conf.d/00-flox.fish
@@ -2,6 +2,6 @@ if type -fq flox
   flox activate -d ~ -m run | source
 
   if not test (pwd) -ef $HOME; and test -d .flox
-    flox activate -m run | source
+    flox activate -m dev | source
   end
 end
