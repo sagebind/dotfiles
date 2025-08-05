341eae788021bc419d0014afda3f135ba8899f9f
Stephen M. Coakley <me@stephencoakley.com>
2025-08-04 20:55:56 -0500

    Remove lla handling

--- home/.config/fish/functions/ls.fish
+++ home/.config/fish/functions/ls.fish
@@ -1,8 +1,8 @@
 function ls -d "Show more detailed 'ls' output" --wraps ls
-  if type -fq lla
-    command lla $argv
-  else if type -fq eza
-    command eza -l $argv
+  if type -fq eza
+    set -x EZA_ICONS_AUTO 1
+
+    command eza --long --no-quotes --git --smart-group --mounts --extended $argv
   else
     command ls -FHl --human-readable --color=always --group-directories-first $argv
   end
