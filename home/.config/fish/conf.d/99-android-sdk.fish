567b525917eb38c0094e205e6bf9c5ff29f087da
Stephen M. Coakley <me@stephencoakley.com>
2025-08-30 12:26:51 -0500

    Remove manually configured android sdk

--- home/.config/fish/conf.d/99-android-sdk.fish
+++
@@ -1,5 +0,0 @@
-if test -d $HOME/.local/android_sdk
-  set -gx ANDROID_HOME $HOME/.local/android_sdk
-  set -gx ANDROID_SDK_ROOT $ANDROID_HOME
-  fish_add_path --global $ANDROID_HOME/cmdline-tools/latest/bin
-end
