96819d0a194447848d32656b71f24c72c1950028
Stephen M. Coakley <me@stephencoakley.com>
2025-08-30 12:29:00 -0500

    Commit last Hyprland setup attempt

--- home.linux/.config/hypr/scripts/resolve.sh
+++ home.linux/.config/hypr/scripts/resolve.sh
@@ -2,7 +2,7 @@
 
 # Some goofiness to get Resolve to work under Hyprland.
 
-exec gamescope -w 2560 -h 1560 -W 2560 -H 1560 -S integer --force-windows-fullscreen \
+exec gamescope -w 2560 -h 1560 -W 2560 -H 1560 -S integer --backend sdl \
     env \
     __NV_PRIME_RENDER_OFFLOAD=1 \
     __VK_LAYER_NV_optimus=NVIDIA_only \
