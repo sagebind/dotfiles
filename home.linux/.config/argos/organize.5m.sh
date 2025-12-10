f1c048730aaa542a63263f4fb5b9527163c29514
Stephen M. Coakley <me@stephencoakley.com>
2025-12-09 20:31:40 -0600

    Cleanup some old configs

--- home.linux/.config/argos/organize.5m.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Organize | iconName=folder-symbolic"
-echo "---"
-echo "Simulate | bash=organize param1=sim terminal=true"
-echo "Run | bash=organize param1=run terminal=true"
--- home.linux/.config/argos/sys-temp.30s.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-ACPI_TEMP=$(sensors -j 2>/dev/null | jq -r '.["acpitz-acpi-0"].temp1.temp1_input | round')
-
-echo "<small>$ACPI_TEMP °C</small> | iconName=temperature-symbolic"
