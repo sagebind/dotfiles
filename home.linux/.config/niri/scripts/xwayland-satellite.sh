057e9cfeb05b50acea1290f8ac8683f9f827cba7
Stephen M. Coakley <me@stephencoakley.com>
2026-01-17 11:21:55 -0600

    xwayland-satellite backtraces

--- home.linux/.config/niri/scripts/xwayland-satellite.sh
+++ home.linux/.config/niri/scripts/xwayland-satellite.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+set -eu
 
 # Wrapper around xwayland-satellite that captures output to a log file to aid in debugging.
 
 LOG_DIR="$HOME/.local/var/xwayland-satellite"
 mkdir -p "$LOG_DIR"
-exec xwayland-satellite "$@" >>$LOG_DIR/stdout.log 2>>$LOG_DIR/stderr.log
+
+export RUST_BACKTRACE=full
+export RUST_LIB_BACKTRACE=1
+
+xwayland-satellite "$@" >>$LOG_DIR/stdout.log 2>>$LOG_DIR/stderr.log
