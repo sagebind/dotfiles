a027d6d36cce8ab90a8c07fd3e0bc5d13908e38e
Stephen M. Coakley <me@stephencoakley.com>
2026-01-31 15:29:17 -0600

    More clever broot integration

--- home/.config/fish/conf.d/99-broot.fish
+++ home/.config/fish/conf.d/99-broot.fish
@@ -1,3 +1,11 @@
 if type -fq broot
   broot --print-shell-function fish | source -
+
+  # Create a version of `br` that will append the selected path to the current
+  # command line buffer if broot is exited with `:print_path` or similar commands.
+  function _broot_commandline
+    if br | read --line -l out
+      commandline --insert $out
+    end
+  end
 end
