7eb8310cd6a145641a58807669029a7c5ff3ff2d
Stephen M. Coakley <me@stephencoakley.com>
2026-02-17 23:47:37 -0600

    Vicinae scripts

---
+++ home.linux/.local/share/vicinae/scripts/organize-sim.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# @vicinae.schemaVersion 1
+# @vicinae.title Organize Simulate
+# @vicinae.mode fullOutput
+
+# Disable progress bars which don't work inside Vicinae.
+export TERM=dumb
+
+organize sim
---
+++ home.linux/.local/share/vicinae/scripts/organize.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# @vicinae.schemaVersion 1
+# @vicinae.title Organize
+# @vicinae.mode fullOutput
+
+# Disable progress bars which don't work inside Vicinae.
+export TERM=dumb
+
+organize run
