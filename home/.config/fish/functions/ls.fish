9037eec38e59ad40659a49bf9794024e028097d6
Stephen M. Coakley <me@stephencoakley.com>
2026-04-30 10:23:10 -0500

    ls wrapper improvements

--- home/.config/fish/functions/ls.fish
+++ home/.config/fish/functions/ls.fish
@@ -1,9 +1,19 @@
 function ls -d "Show more detailed 'ls' output" --wraps ls
-  if type -fq eza
-    set -x EZA_ICONS_AUTO 1
+  if status is-interactive
+    if type -fq eza
+      set eza_args --long --no-quotes --git --group-directories-first --mounts
+      set -x EZA_ICONS_AUTO 1
 
-    command eza --long --no-quotes --git --smart-group --mounts --extended $argv
+      # Extended args are noisy on macOS
+      if status build-info | not string match -q '*apple*'
+        set --append eza_args --extended
+      end
+
+      command eza $eza_args $argv
+    else
+      command ls -FHl --human-readable --color=always --group-directories-first $argv
+    end
   else
-    command ls -FHl --human-readable --color=always --group-directories-first $argv
+    command ls $argv
   end
 end
