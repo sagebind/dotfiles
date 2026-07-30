1f7f5bc1c315917b972bb90ea2ac0c203e8278f0
Stephen M. Coakley <me@stephencoakley.com>
2026-07-30 14:16:47 -0500

    Automatically open Zed tabs in current window if invoked inside a Zed terminal

--- config/fish/functions/zed.fish
+++ config/fish/functions/zed.fish
@@ -1,9 +1,15 @@
 function zed --wraps zed
-  if set -q argv[1]
-    command zed $argv
+  set -l args $argv
+
+  if test $TERM_PROGRAM = zed; and not contains -- --existing $args
+    set --prepend args --existing
+  end
+
+  if set -q args[1]
+    command zed $args
   else if not isatty stdin
     command zed -
   else
-    command zed -n $PWD
+    command zed --new $PWD
   end
 end
