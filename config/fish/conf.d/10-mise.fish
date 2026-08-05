12b2a5200baefa0d773129f9e9f8469584779ff2
Stephen M. Coakley <me@stephencoakley.com>
2026-08-05 11:07:04 -0500

    Move macbook detection to precomputed miserc

--- config/fish/conf.d/10-mise.fish
+++ config/fish/conf.d/10-mise.fish
@@ -1,8 +1,4 @@
 if type -fq mise
-  if type -fq hostname; and hostname | string match -q "MacBook-Pro-StephenCoakley"
-    set -gx MISE_ENV widen
-  end
-
   mise activate fish | source
   mise completion fish | source
 end
