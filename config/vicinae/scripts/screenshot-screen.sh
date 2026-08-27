751f8a95e698898d87d9a6319669fd58f5281edc
Stephen M. Coakley <me@stephencoakley.com>
2026-08-26 20:26:12 -0500

    Fix Vicinae window being captured in screenshots

--- config/vicinae/scripts/screenshot-screen.sh
+++ config/vicinae/scripts/screenshot-screen.sh
@@ -3,4 +3,4 @@
 # @vicinae.title Screenshot Screen
 # @vicinae.mode silent
 
-niri msg action screenshot-screen
+niri msg action spawn-sh -- "sleep 2 && niri msg action screenshot-screen"
--- config/vicinae/scripts/screenshot-window.sh
+++ config/vicinae/scripts/screenshot-window.sh
@@ -3,4 +3,4 @@
 # @vicinae.title Screenshot Window
 # @vicinae.mode silent
 
-niri msg action screenshot-window
+niri msg action spawn-sh -- "sleep 2 && niri msg action screenshot-window"
