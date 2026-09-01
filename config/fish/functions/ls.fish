fd03d085f301c8f0b8669cefe7588fbbe9b90285
Stephen M. Coakley <me@stephencoakley.com>
2026-09-01 18:10:21 -0500

    Extended attrs are noisy even on linux now

--- config/fish/functions/ls.fish
+++ config/fish/functions/ls.fish
@@ -3,12 +3,6 @@ function ls -d "Show more detailed 'ls' output" --wraps ls
     if type -fq eza
       set eza_args --long --no-quotes --git --group-directories-first --mounts
       set -x EZA_ICONS_AUTO 1
-
-      # Extended args are noisy on macOS
-      if status build-info | not string match -q '*apple*'
-        set --append eza_args --extended
-      end
-
       command eza $eza_args $argv
     else
       command ls -FHl --human-readable --color=always --group-directories-first $argv
