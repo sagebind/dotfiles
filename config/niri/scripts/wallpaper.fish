e927405650afaa75a4c665f3af9a34500098086d
Stephen M. Coakley <me@stephencoakley.com>
2026-09-26 10:43:09 -0500

    Update wallpaper.fish

--- config/niri/scripts/wallpaper.fish
+++ config/niri/scripts/wallpaper.fish
@@ -3,5 +3,5 @@
 set -l wallpaper_path "/mnt/data/Seafile/Wallpapers/Animated/hornet-green-cave-hollow-knight-silksong-moewalls-com.mp4"
 
 if type -fq phonto
-  igpu-run phonto --pause-below 30 $wallpaper_path
+  # igpu-run phonto --pause-below 30 $wallpaper_path
 end
